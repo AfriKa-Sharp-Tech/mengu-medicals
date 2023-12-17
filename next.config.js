350e658c146682f54322d996339f4493e9aacaad
Jume <jumeb@Jumes-Mac.local>
2023-12-17 21:42:12 +0100

    More on implementation

--- next.config.js
+++ next.config.js
@@ -4,26 +4,8 @@ const API_BASE_ENDPOINT = process.env.NEXT_PUBLIC_BASE_URL;
 
 const nextConfig = {
   output: "export",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:slug*",
-        destination: `${API_BASE_ENDPOINT}/:slug*`,
-      },
-    ];
-  },
+  basePath: '/mengu-medicals',
+
 };
 
 module.exports = nextConfig;
